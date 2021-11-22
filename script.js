2860e2f48a9a16b941bf643114baa00d159fc658
alescargot <alexia.jechoux@gmail.com>
2021-11-22 09:45:38 +0100

    hello

---
+++ script.js
@@ -0,0 +1,314 @@
+const case1 = document.querySelector("[title='1']");
+const case2 = document.querySelector("[title='2']");
+const case3 = document.querySelector("[title='3']");
+const case4 = document.querySelector("[title='4']");
+const case5 = document.querySelector("[title='5']");
+const case6 = document.querySelector("[title='6']");
+const case7 = document.querySelector("[title='7']");
+const case8 = document.querySelector("[title='8']");
+const case9 = document.querySelector("[title='9']");
+const case10 = document.querySelector("[title='10']");
+const case11 = document.querySelector("[title='11']");
+const case12 = document.querySelector("[title='12']");
+const case13 = document.querySelector("[title='13']");
+const case14 = document.querySelector("[title='14']");
+const case15 = document.querySelector("[title='15']");
+const case16 = document.querySelector("[title='16']");
+const case17 = document.querySelector("[title='17']");
+const case18 = document.querySelector("[title='18']");
+const case19 = document.querySelector("[title='19']");
+const case20 = document.querySelector("[title='20']");
+const case21 = document.querySelector("[title='21']");
+const case22 = document.querySelector("[title='22']");
+const case23 = document.querySelector("[title='23']");
+const case24 = document.querySelector("[title='24']");
+
+var todayDate = new Date();
+var dateDay1 = new Date(2021, 10, 1, 0, 1, 0);
+
+var dateDay2 = new Date();
+dateDay2.setDate(dateDay1.getDate() + 1);
+
+var dateDay3 = new Date();
+dateDay3.setDate(dateDay2.getDate() + 1);
+
+var dateDay4 = new Date();
+dateDay4.setDate(dateDay3.getDate() + 1);
+
+var dateDay5 = new Date();
+dateDay5.setDate(dateDay4.getDate() + 1);
+
+var dateDay6 = new Date();
+dateDay6.setDate(dateDay5.getDate() + 1);
+
+var dateDay7 = new Date();
+dateDay7.setDate(dateDay6.getDate() + 1);
+
+var dateDay8 = new Date();
+dateDay8.setDate(dateDay7.getDate() + 1);
+
+var dateDay9 = new Date();
+dateDay9.setDate(dateDay8.getDate() + 1);
+
+var dateDay10 = new Date();
+dateDay10.setDate(dateDay9.getDate() + 1);
+
+var dateDay11 = new Date();
+dateDay11.setDate(dateDay10.getDate() + 1);
+
+var dateDay12 = new Date();
+dateDay12.setDate(dateDay11.getDate() + 1);
+
+var dateDay13 = new Date();
+dateDay13.setDate(dateDay12.getDate() + 1);
+
+var dateDay14 = new Date();
+dateDay14.setDate(dateDay13.getDate() + 1);
+
+var dateDay15 = new Date();
+dateDay15.setDate(dateDay14.getDate() + 1);
+
+var dateDay16 = new Date();
+dateDay16.setDate(dateDay15.getDate() + 1);
+
+var dateDay17 = new Date();
+dateDay17.setDate(dateDay16.getDate() + 1);
+
+var dateDay18 = new Date();
+dateDay18.setDate(dateDay17.getDate() + 1);
+
+var dateDay19 = new Date();
+dateDay19.setDate(dateDay18.getDate() + 1);
+
+var dateDay20 = new Date();
+dateDay20.setDate(dateDay19.getDate() + 1);
+
+var dateDay21 = new Date();
+dateDay21.setDate(dateDay20.getDate() + 1);
+
+var dateDay22 = new Date();
+dateDay22.setDate(dateDay21.getDate() + 1);
+
+var dateDay23 = new Date();
+dateDay23.setDate(dateDay22.getDate() + 1);
+
+var dateDay24 = new Date();
+dateDay24.setDate(dateDay23.getDate() + 1);
+
+case1.addEventListener("click", () => {
+  if (todayDate < dateDay1) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay1) {
+    window.open("images/1.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='1']>iframe").style.display = "block";
+  }
+});
+
+case2.addEventListener("click", () => {
+  if (todayDate < dateDay2) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay2) {
+    window.open("images/2.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='2']>iframe").style.display = "block";
+  }
+});
+
+case3.addEventListener("click", () => {
+  if (todayDate < dateDay3) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay3) {
+    window.open("images/3.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='3']>iframe").style.display = "block";
+  }
+});
+
+case4.addEventListener("click", () => {
+  if (todayDate < dateDay4) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay4) {
+    window.open("images/4.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='4']>iframe").style.display = "block";
+  }
+});
+
+case5.addEventListener("click", () => {
+  if (todayDate < dateDay5) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay5) {
+    window.open("images/5.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='5']>iframe").style.display = "block";
+  }
+});
+
+case6.addEventListener("click", () => {
+  if (todayDate < dateDay6) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay6) {
+    window.open("images/6.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='6']>iframe").style.display = "block";
+  }
+});
+
+case7.addEventListener("click", () => {
+  if (todayDate < dateDay7) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay7) {
+    window.open("images/7.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='7']>iframe").style.display = "block";
+  }
+});
+
+case8.addEventListener("click", () => {
+  if (todayDate < dateDay8) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay8) {
+    window.open("images/8.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='8']>iframe").style.display = "block";
+  }
+});
+
+case9.addEventListener("click", () => {
+  if (todayDate < dateDay9) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay9) {
+    window.open("images/9.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='9']>iframe").style.display = "block";
+  }
+});
+
+case10.addEventListener("click", () => {
+  if (todayDate < dateDay10) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay10) {
+    window.open("images/10.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='10']>iframe").style.display = "block";
+  }
+});
+
+case11.addEventListener("click", () => {
+  if (todayDate < dateDay11) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay11) {
+    window.open("images/11.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='11']>iframe").style.display = "block";
+  }
+});
+
+case12.addEventListener("click", () => {
+  if (todayDate < dateDay12) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay12) {
+    window.open("images/12.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='12']>iframe").style.display = "block";
+  }
+});
+
+case13.addEventListener("click", () => {
+  if (todayDate < dateDay13) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay13) {
+    window.open("images/13.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='13']>iframe").style.display = "block";
+  }
+});
+
+case14.addEventListener("click", () => {
+  if (todayDate < dateDay14) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay14) {
+    window.open("images/14.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='14']>iframe").style.display = "block";
+  }
+});
+
+case15.addEventListener("click", () => {
+  if (todayDate < dateDay15) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay15) {
+    window.open("images/15.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='15']>iframe").style.display = "block";
+  }
+});
+
+case16.addEventListener("click", () => {
+  if (todayDate < dateDay16) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay16) {
+    window.open("images/16.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='16']>iframe").style.display = "block";
+  }
+});
+
+case17.addEventListener("click", () => {
+  if (todayDate < dateDay17) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay17) {
+    window.open("images/17.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='17']>iframe").style.display = "block";
+  }
+});
+
+case18.addEventListener("click", () => {
+  if (todayDate < dateDay18) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay18) {
+    window.open("images/18.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='18']>iframe").style.display = "block";
+  }
+});
+
+case19.addEventListener("click", () => {
+  if (todayDate < dateDay19) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay19) {
+    window.open("images/19.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='19']>iframe").style.display = "block";
+  }
+});
+
+case20.addEventListener("click", () => {
+  if (todayDate < dateDay20) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay20) {
+    window.open("images/20.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='20']>iframe").style.display = "block";
+  }
+});
+
+case21.addEventListener("click", () => {
+  if (todayDate < dateDay21) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay21) {
+    window.open("images/21.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='21']>iframe").style.display = "block";
+  }
+});
+
+case22.addEventListener("click", () => {
+  if (todayDate < dateDay22) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay22) {
+    window.open("images/22.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='22']>iframe").style.display = "block";
+  }
+});
+
+case23.addEventListener("click", () => {
+  if (todayDate < dateDay23) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay23) {
+    window.open("images/23.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='23']>iframe").style.display = "block";
+  }
+});
+
+case24.addEventListener("click", () => {
+  if (todayDate < dateDay24) {
+    alert("Too soon !");
+  } else if (todayDate >= dateDay24) {
+    window.open("images/24.jpg", "fullscreen=yes", "toolbar=yes");
+    document.querySelector("[title='24']>iframe").style.display = "block";
+  }
+});
+
+//pop up
